ad34a396a58c1055c316bf5a37e530078fcece0c
ImTiara <39366953+ImTiara@users.noreply.github.com>
2020-07-16 12:35:45 +0200

    Initial commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(CleanConsole.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(CleanConsole.BuildInfo.Company)]
+[assembly: AssemblyProduct(CleanConsole.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + CleanConsole.BuildInfo.Author)]
+[assembly: AssemblyTrademark(CleanConsole.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(CleanConsole.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(CleanConsole.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(CleanConsole.CleanConsole), CleanConsole.BuildInfo.Name, CleanConsole.BuildInfo.Version, CleanConsole.BuildInfo.Author, CleanConsole.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonModGame("VRChat", "VRChat")]
---
+++ CleanConsole.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace CleanConsole
+{
+    public static class BuildInfo
+    {
+        public const string Name = "CleanConsole";
+        public const string Author = "ImTiara";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class CleanConsole : MelonMod
+    {
+        public unsafe override void OnApplicationStart()
+        {
+            string[] methods = {
+                "NativeMethodInfoPtr_WriteLine_Public_Static_Void_Object_0",
+                "NativeMethodInfoPtr_WriteLine_Public_Static_Void_String_0",
+                "NativeMethodInfoPtr_WriteLine_Public_Static_Void_String_Object_Object_0"
+            };
+
+            foreach(string name in methods)
+                Hook((IntPtr)(&*(IntPtr*)(IntPtr)typeof(Il2CppSystem.Console).GetField(name,
+                    BindingFlags.NonPublic | BindingFlags.Static).GetValue(null)),
+                    Marshal.GetFunctionPointerForDelegate(new Action(() => { })));
+
+        }
+
+        public static void Hook(IntPtr _old, IntPtr _new) => typeof(Imports).GetMethod("Hook", Harmony.AccessTools.all).Invoke(null, new object[] { _old, _new });
+    }
+}
